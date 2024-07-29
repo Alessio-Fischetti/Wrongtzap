d547829e11f6beeca90eac9e440c444332604f03
mattia33011 <mattia.iaria30@gmail.com>
2024-07-30 01:53:26 +0200

    creazione di un service

--- src/app/components/folder/folder.page.ts
+++ src/app/components/folder/folder.page.ts
@@ -1,6 +1,7 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { IonHeader, IonToolbar, IonButtons, IonMenuButton, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-folder',
@@ -12,9 +13,40 @@ import { IonHeader, IonToolbar, IonButtons, IonMenuButton, IonTitle, IonContent 
 export class FolderPage implements OnInit {
   public folder!: string;
   private activatedRoute = inject(ActivatedRoute);
-  constructor() {}
+  constructor(private userService: UserService) {}
 
-  ngOnInit() {
+  result: any
+
+  jsonResult(){
+    return JSON.stringify(this.result)
+  }
+
+  async ngOnInit() {
+    this.loading = true
     this.folder = this.activatedRoute.snapshot.paramMap.get('id') as string;
+    await this.getUser('1')
+    this.loading = false
+  }
+
+  loading = false
+  async getUser(id: string){
+    this.loading = true
+    this.result = await this.getUserById(id)
+    this.loading = false
+  }
+
+  getUserById(id: string){
+    return new Promise<any>(resolve => {
+      this.userService.getUserById(id).subscribe({
+        next: (result) => {
+          resolve(result)
+        },
+        error: (err) => {
+          console.log(err);
+          resolve(undefined)          
+        },
+        complete: () => {}
+      })
+      })
   }
 }
